43de857d909cd73f9bb952f477fa8b926c3be379
Thahyana Costa <thaycostalima@gmail.com>
2025-08-24 05:28:24 +0000

    att JS

---
+++ script.js
@@ -0,0 +1,3 @@
+function goToContact() {
+    window.location.href = 'https://wa.me/5585981070550?text=Ol%C3%A1%20seja%20bem%20vindo%20a%20Pixel%20Box%20%F0%9F%8C%90.%20Aqui%20voc%C3%AA%20encontra%20o%20melhor%20da%20tecnologia.%20Em%20breve%20iremos%20lhe%20atender';
+}
